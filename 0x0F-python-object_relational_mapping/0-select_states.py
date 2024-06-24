1a5289fdbe34e6a6629767b9d4a94fd90d498cc5
jones0chikwezga <joneschikwezga4@gmail.com>
2024-06-23 23:49:45 -0700

    0-select_states.py edt

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,39 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
-import sys
 import MySQLdb
+import sys
+
+def list_states(username, password, database):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Execute the query to select all states sorted by id
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the rows in the result of the query
+    rows = cursor.fetchall()
+
+    # Print each row
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
 
 if __name__ == "__main__":
-        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-            cur = db.cursor()
-                cur.execute("SELECT * FROM `states`")
-                    _states = cur.fetchall()
-                        for state in _states:
-                                    print(state)
+    # Get the command line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # List states from the database
+    list_states(username, password, database)
